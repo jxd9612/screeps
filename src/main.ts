0b04737f137c31abe05be4c29ae360a02f510d00
jxd9612 <15916537217@163.com>
2020-10-19 22:23:02 +0800

    v20.10.19

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+// module.exports.loop = function (): void {
+//     console.log('typecript');
+// }
+
+function hello(compiler: string) {
+    console.log(`Hello from ${compiler}`);
+}
+
+hello("TypeScript");
