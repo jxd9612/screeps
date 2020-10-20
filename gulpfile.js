b264456c01fedb5259e9a41f62eb3b4840ea3175
jxd9612 <15916537217@163.com>
2020-10-20 20:38:58 +0800

    v20-10-20

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
-const gulp = require("gulp");
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
- 
-gulp.task("default", function () {
-    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const screeps = require('gulp-screeps');
+const config = require('./config/index');
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('screeps', done => {
+    gulp.src('dist/**/*.js').pipe(screeps(config));
+    done();
 });
+
+gulp.task('resolve', done => {
+    tsProject.src().pipe(tsProject()).js.pipe(gulp.dest('dist'));
+    done();
+});
+ 
+gulp.task('default', gulp.series('resolve', 'screeps', done => {
+    done();
+}));
